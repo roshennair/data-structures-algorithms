4650646eafced804e095760ef9c9529e7c370a6d
Roshen Nair <roshen620@gmail.com>
2020-08-04 23:20:51 +0800

    Added new javascript folder
    This new javascript folder will hold all implementations coded in JavaScript.

---
+++ javascript/array.js
@@ -0,0 +1,12 @@
+/**
+ * A basic implementation of a dynamic array data structure in JS
+ * 
+ * Methods to implement: access, push, pop, delete 
+ */
+
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+}
