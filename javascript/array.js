e3d6d549c2155a98fe85a289d68163d31fda99a6
Roshen Nair <roshen620@gmail.com>
2020-08-05 22:53:28 +0800

    Completed array.js
    Completed and tested all methods of MyArray class

--- javascript/array.js
+++ javascript/array.js
@@ -1,41 +1,93 @@
+/**
+ * - My custom implementation of an array data structure via a JS class
+ * - For illustration purposes only, this implementation uses a JS object to store the array elements
+ * - Overall space complexity: O(n), n = Length of array 
+ */
 class MyArray {
     // Instantiates a new empty array
+    // Time complexity: O(1)
+    // Space complexity: O(1)
     constructor() {
         this.data = {};
         this.length = 0;
     }
 
     // Returns the element at a given index
+    // Time complexity: O(1)
+    // Space complexity: O(1)
     lookup(index) {
         if (index < 0 || index >= this.length) throw new Error(`Index ${index} is out of bounds.`);
+        
         return this.data[index];
     }
 
     // Inserts an element at the end of the array
+    // Time complexity: O(1)
+    // Space complexity: O(1)
     push(element) {
         if (element === undefined) throw new Error('This function requires 1 argument: element.');
+        
         this.data[this.length] = element;
+        this.length++;
+        return this.length;
     }
 
     // Removes the element at the end of the array
+    // Time complexity: O(1)
+    // Space complexity: O(1)
     pop() {
         if (this.length < 1) throw new Error('This array is empty.');
+        
+        const deletedElement = this.data[this.length - 1]; 
         delete this.data[this.length - 1];
         this.length--;
+        return deletedElement;
+    }
+
+    // Inserts an element at a given index
+    // Time complexity: O(n), n = Length of array
+    // Space complexity: O(1)
+    insert(element, index) {
+        if (element === undefined) {
+            throw new Error('This method requires an element argument');
+        } else if (index < 0 || index >= this.length) {
+            throw new Error(`Index ${index} is out of bounds.`);
+        } else if (index === this.length - 1) {
+            return this.push(element);
+        }
+
+        // Shift each index to the left, keeping track of next element
+        let currentElement = element;
+        for (let i = index; i < this.length; i++) {
+            let temp = this.data[i];
+            this.data[i] = currentElement;
+            currentElement = temp;
+        }
+        // Create new index for last element
+        this.data[this.length] = currentElement;
+        this.length++;
+        return this.length;
     }
     
     // Removes the element at a given index
+    // Time complexity: O(n), n = Length of array
+    // Space complexity: O(1)
     delete(index) {
-        if (index < 0 || index >= this.length) throw new Error(`Index ${index} is out of bounds.`);
-        delete this.data[index];
-        this.shiftIndexes(index);
-    }
-
-    shiftIndexes(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error(`Index ${index} is out of bounds.`);
+        } else if (index === this.length - 1) {
+            return this.pop();
+        }
+        
+        let deletedElement = this.data[index];
+        delete this.data[index]; // Remove selected element
+        // Shift each index to the right
         for (let i = index; i < this.length - 1; i++) {
             this.data[i] = this.data[i + 1];
         }
+        // Remove last unused index
         delete this.data[this.length - 1];
         this.length--;
+        return deletedElement;
     }
 }
