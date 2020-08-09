9c14db51c7f48b62e273c65521143e5166621188
Roshen Nair <roshen620@gmail.com>
2020-08-09 16:16:00 +0800

    Created data-structures/templates
    Created a new directory storing templates for data structure implementations in various languages

---
+++ data-structures/templates/template.js
@@ -0,0 +1,23 @@
+/**
+ * - My implementation of a  as a JS class
+ * - Overall space complexity: O()
+ */
+class DataStructureTemplate {
+    /**
+     * - Instantiates a new 
+     * - Time complexity: O()
+     * - Space complexity: O()
+     */
+    constructor(size) {
+        
+    }
+
+    /**
+     * - Function
+     * - Time complexity: O()
+     * - Space complexity: O()
+     */
+    method(key) {
+        
+    }
+}
