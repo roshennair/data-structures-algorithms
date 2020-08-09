5a9dadd35c582de4dad34ae26f0029fcbe340450
Roshen Nair <roshen620@gmail.com>
2020-08-09 16:32:15 +0800

    Created singly-linked-lists/ and doubly-linked-lists/
    Created 2 directories to separate implementations of SLLs and DLLs

---
+++ data-structures/linked-lists/singly-linked-lists/linkedList.js
@@ -0,0 +1,23 @@
+/**
+ * - My implementation of a singly linked list as a JS class
+ * - Overall space complexity: O(n)
+ */
+class SinglyLinkedList {
+    /**
+     * - Instantiates a new singly linked list
+     * - Time complexity: O()
+     * - Space complexity: O()
+     */
+    constructor(size) {
+        
+    }
+
+    /**
+     * - Function
+     * - Time complexity: O()
+     * - Space complexity: O()
+     */
+    method(key) {
+        
+    }
+}
